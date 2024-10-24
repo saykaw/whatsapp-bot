44d9d66e4002a92f2dc5da33eb0aba2da5255df9
Shreyas V. Bhoir <131248206+ShrAI-yes@users.noreply.github.com>
2024-10-24 22:03:20 +0530

    Delete testing.py

--- testing.py
+++
@@ -1,9 +0,0 @@
-from db_store import Database
-
-store = Database()
-
-doc_ref = store.init_user("123981290","82902817890","Sayali")
-
-msg = store.payload("Sayali","It worked!!!!", "109381208")
-
-store.add_convo(doc_ref,msg)
